bc6a9c3815815148f2a75b85d77c3f66df8dd58d
Tanuja <tanuja.jain01@gmail.com>
2021-01-20 12:53:15 +0530

    2nd class adding order

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
 	path('',views.index_view,name='index_view'),
 	path('add/product/',views.add_product_view,name='add_product_view'),
+	path('all/product/',views.all_product_view,name='all_product_view'),
+	path('add/order/',views.add_order_view,name='add_order_view'),
+	path('check/quant/',views.check_quant,name='check_quant'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import *
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 '''
@@ -25,6 +27,9 @@ Product.objects.filter(
 def index_view(request):
 	return render(request,'index.html')
 
+def all_product_view(request):
+	products = Product.objects.all()
+	return render(request,'all_product.html',{'products':products})
 
 def add_product_view(request):
 	if request.method=='POST':
@@ -32,6 +37,9 @@ def add_product_view(request):
 		product_price = request.POST.get('product_price')
 		product_quant = request.POST.get('product_quant')
 		
+		if product_name=='':
+			return render(request,'add_product.html',{'message':'Product Name cannot be blank'})
+
 		Product.objects.create(
 			product_name=product_name,
 			price = product_price,
@@ -42,3 +50,39 @@ def add_product_view(request):
 	else:
 		return render(request,'add_product.html')
 
+
+
+def add_order_view(request):
+	if request.method=='POST':
+		prod_id = request.POST.get('prod_id')
+		purchase_quant = int(request.POST.get('purchase_quant'))
+		prod = Product.objects.filter(id=prod_id)
+		if prod:
+			avail_quant = prod[0].quantity-purchase_quant
+			prod.update(quantity=avail_quant)
+
+			Order.objects.create(
+				product=prod[0],
+				quantity_purchased = purchase_quant)
+			return redirect('all_product_view')
+	else:
+		product_id = request.GET.get('id')	
+		product = Product.objects.filter(id=product_id)
+		print(product)
+		if product:
+			return render(request,'add_order.html',{'product':product[0]})
+		else:
+			return redirect('all_product_view')
+
+@csrf_exempt
+def check_quant(request):
+	input_quant = request.POST.get('input_quant')
+	produc_id = request.POST.get('produc_id')
+
+	#to check whether any product of that id exists or not
+	product = Product.objects.filter(id=produc_id,quantity__gte=input_quant) 
+	if not product:
+		return JsonResponse({'status':'Error error occured. Check your quantity'})
+
+	else:
+		return JsonResponse({'status':'success'})
